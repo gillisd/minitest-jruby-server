15a12abd26d42a7235069754894e1ce6b7154b0e
David Gillis <david@flipmine.com>
2026-04-10 04:14:00 +0000

    feat: error hierarchy and Config with sensible defaults

---
+++ test/minitest/jruby_server/test_config.rb
@@ -0,0 +1,54 @@
+require "test_helper"
+require "tmpdir"
+
+class TestConfig < Minitest::Test
+  def test_default_project_root_is_pwd
+    config = Minitest::JRubyServer::Config.new
+    assert_equal Dir.pwd, config.project_root
+  end
+
+  def test_custom_project_root
+    config = Minitest::JRubyServer::Config.new(project_root: "/tmp/myproject")
+    assert_equal "/tmp/myproject", config.project_root
+  end
+
+  def test_default_uri_file_in_project_root
+    config = Minitest::JRubyServer::Config.new(project_root: "/tmp/myproject")
+    assert_equal "/tmp/myproject/.minitest-jruby.uri", config.uri_file
+  end
+
+  def test_custom_uri_file
+    config = Minitest::JRubyServer::Config.new(uri_file: "/custom/path.uri")
+    assert_equal "/custom/path.uri", config.uri_file
+  end
+
+  def test_default_test_paths
+    config = Minitest::JRubyServer::Config.new
+    assert_equal ["test/**/*_test.rb", "test/**/test_*.rb"], config.test_paths
+  end
+
+  def test_default_load_paths
+    config = Minitest::JRubyServer::Config.new
+    assert_equal ["lib", "test"], config.load_paths
+  end
+
+  def test_default_connect_timeout
+    config = Minitest::JRubyServer::Config.new
+    assert_equal 5, config.connect_timeout
+  end
+
+  def test_socket_dir_defaults_to_tmpdir
+    config = Minitest::JRubyServer::Config.new
+    assert_equal Dir.tmpdir, config.socket_dir
+  end
+
+  def test_socket_path_includes_pid
+    config = Minitest::JRubyServer::Config.new
+    assert_match(/minitest_jruby\.\d+$/, config.socket_path)
+  end
+
+  def test_uri_includes_drbunix_scheme
+    config = Minitest::JRubyServer::Config.new
+    assert config.uri.start_with?("drbunix:")
+  end
+end
